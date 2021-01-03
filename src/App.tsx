2524d1399a75b1642d9689b82fa9cb159d2e4ea4
Pablo <mendez4a@gmail.com>
2021-01-03 13:56:08 +0100

    React routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,37 +1,51 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { transform } from "typescript";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Projects from "./Projects";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   return (
-    <div>
-      <nav>
-        <div className="logo">React nav</div>
-        <ul
-          className="nav-links"
-          style={{ transform: open ? "translateX(0px)" : "" }}
-        >
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About me</a>
-          </li>
-          <li>
-            <a>Projects</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-        </ul>
-        <FontAwesomeIcon
-          onClick={() => setOpen(!open)}
-          className="burger"
-          icon={faBars}
-        />
-      </nav>
-    </div>
+    <>
+      <div>
+        <nav>
+          <div className="logo">Pablo</div>
+          <ul
+            className="nav-links"
+            style={{ transform: open ? "translateX(0px)" : "" }}
+          >
+            <li>
+              <NavLink className="nav-link" to="/" exact>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/about">
+                About me
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/projects">
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <FontAwesomeIcon
+            onClick={() => setOpen(!open)}
+            className="burger"
+            icon={faBars}
+          />
+        </nav>
+      </div>
+    </>
   );
 }
