5f23201a5cb85cbff86c96905d585e831d08098c
Pablo <mendez4a@gmail.com>
2021-01-03 13:21:08 +0100

    Navigation bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { transform } from "typescript";
 
 export default function NavBar() {
-  return <div>NavBar page</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <nav>
+        <div className="logo">React nav</div>
+        <ul
+          className="nav-links"
+          style={{ transform: open ? "translateX(0px)" : "" }}
+        >
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>About me</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+        <FontAwesomeIcon
+          onClick={() => setOpen(!open)}
+          className="burger"
+          icon={faBars}
+        />
+      </nav>
+    </div>
+  );
 }
--- src/components/SinglePost.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function SinglePost() {
-  return <div>single post</div>;
-}
